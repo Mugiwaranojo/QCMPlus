583f3f04ae97dc04aca16ad851bc9855d4fbf5ee
Mugiwaranojo <joan.g.francois@gmail.com>
2014-07-29 14:44:12 +0200

    Ajout de manager pour l'api vers parse.com

--- QCMPlusStudent/app/src/main/java/mydevmind/com/qcmplusstudent/fragment/LoginFragment.java
+++ QCMPlusStudent/app/src/main/java/mydevmind/com/qcmplusstudent/fragment/LoginFragment.java
@@ -1,26 +1,31 @@
-package mydevmind.com.qcmplusstudent.fragment;
+package mydevmind.com.qcmplusstudent.Fragment;
 
 import android.app.Fragment;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import mydevmind.com.qcmplusstudent.APIService.MCQServiceManager;
 import mydevmind.com.qcmplusstudent.R;
 
 /**
- * Created by Joan on 28/07/2014.
+ * Created by Joan on 29/07/2014.
  */
 public class LoginFragment extends Fragment {
 
-    public  LoginFragment(){
+    private EditText loginField, passwordField;
+    private Button loginButton;
 
-    }
+    private MCQServiceManager apiManager;
+
+    private ProgressDialog spinner;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
-        return rootView;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v= inflater.inflate(R.layout.f)
     }
 }
